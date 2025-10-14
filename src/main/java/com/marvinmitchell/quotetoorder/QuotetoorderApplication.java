1dbf1a36e1a714e982728a7faf4e9307ec4d3e6b
Marvin Mitchell <marvinmarpol@gmail.com>
2025-10-14 22:28:43 +0700

    initial commit

---
+++ src/main/java/com/marvinmitchell/quotetoorder/QuotetoorderApplication.java
@@ -0,0 +1,13 @@
+package com.marvinmitchell.quotetoorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuotetoorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuotetoorderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marvinmitchell/quotetoorder/QuotetoorderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marvinmitchell.quotetoorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuotetoorderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
