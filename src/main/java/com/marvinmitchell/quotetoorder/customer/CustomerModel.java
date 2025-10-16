2cbdee366ee085b73063847af5d70d929a1a3131
Marvin Mitchell <marvinmarpol@gmail.com>
2025-10-16 12:22:22 +0700

    add email and password to customers model

--- src/main/java/com/marvinmitchell/quotetoorder/customer/CustomerModel.java
+++ src/main/java/com/marvinmitchell/quotetoorder/customer/CustomerModel.java
@@ -6,7 +6,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
-
 @Entity
 @Table(name = "customers")
 public class CustomerModel {
@@ -15,10 +14,17 @@ public class CustomerModel {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    private String email;
+    private String password;
 
+    
     public CustomerModel(){}
-    public CustomerModel(String name) {
+    
+    public CustomerModel(Long id, String name, String email, String password) {
+        this.id = id;
         this.name = name;
+        this.email = email;
+        this.password = password;
     }
 
     public Long getId() {
@@ -36,4 +42,21 @@ public class CustomerModel {
     public void setName(String name) {
         this.name = name;
     }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 }
